a9a3509442614e8e868dc50a25364312737d281e
diego.chavez27 <noicedude555@gmail.com>
2026-02-08 21:55:29 -0600

    gyyt

--- quickGame/GameScene.swift
+++ quickGame/GameScene.swift
@@ -11,12 +11,62 @@ import GameplayKit
 class GameScene: SKScene {
     var ball : SKSpriteNode!
     let cam = SKCameraNode()
+    var leftButton: SKSpriteNode!
+    var rightButton: SKSpriteNode!
+    var movingLeft = false
+    var movingRight = false
+    
     override func didMove(to view: SKView) {
         ball = self.childNode(withName: "ball") as! SKSpriteNode
         self.camera = cam
+        // Left button
+            leftButton = SKSpriteNode(color: .blue, size: CGSize(width: 80, height: 80))
+            leftButton.alpha = 0.5
+            leftButton.position = CGPoint(x: -self.size.width/2 + 100, y: -self.size.height/2 + 100)
+            leftButton.zPosition = 100
+            addChild(leftButton)
+
+            // Right button
+            rightButton = SKSpriteNode(color: .green, size: CGSize(width: 80, height: 80))
+            rightButton.alpha = 0.5
+            rightButton.position = CGPoint(x: -self.size.width/2 + 220, y: -self.size.height/2 + 100)
+            rightButton.zPosition = 100
+            addChild(rightButton)
     }
     override func update(_ currentTime: TimeInterval) {
         cam.position = ball.position
+        let speed: CGFloat = 200
+            if movingLeft {
+                ball.position.x -= speed * CGFloat(1.0/60.0)
+            }
+            if movingRight {
+                ball.position.x += speed * CGFloat(1.0/60.0)
+            }
+    }
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch in touches {
+            let location = touch.location(in: self)
+
+            if leftButton.contains(location) {
+                movingLeft = true
+            } else if rightButton.contains(location) {
+                movingRight = true
+            } else {
+                jump() // tap anywhere else = jump
+            }
+        }
+    }
+
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch in touches {
+            let location = touch.location(in: self)
+
+            if leftButton.contains(location) {
+                movingLeft = false
+            } else if rightButton.contains(location) {
+                movingRight = false
+            }
+        }
     }
     func reset(){
         var respawn = SKAction.move(to: CGPoint(x: CGFloat.random(in: -320...320), y: 640), duration: 2)
@@ -26,6 +76,8 @@ class GameScene: SKScene {
         var burst = SKAction.move(by: CGVector(dx: 0, dy: 200), duration: 1)
         ball.run(burst)
     }
-    
+    /*override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        jump()
+    }*/
     
 }
--- quickGame/Joystick.swift
+++ quickGame/Joystick.swift
@@ -6,3 +6,44 @@
 //
 
 import Foundation
+import SpriteKit
+
+class Joystick: SKNode{
+    var base: SKShapeNode!
+    var knob: SKShapeNode!
+    var radius: CGFloat = 60
+    
+    
+    override init() {
+        
+        super.init()
+        
+        base = SKShapeNode(circleOfRadius: radius)
+        base.fillColor = .gray
+        base.alpha = 0.4
+        base.zPosition = 1
+        knob = SKShapeNode(circleOfRadius: radius / 2)
+        knob.fillColor = .white
+        knob.zPosition = 2
+        addChild(base)
+        addChild(knob)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func moveJoystick(to position: CGPoint) {
+        let distance = sqrt(position.x * position.x + position.y * position.y)
+        if distance <= radius{
+            knob.position = position
+        } else {
+            let angle = atan2(position.y, position.x)
+            knob.position = CGPoint(
+                x: cos(angle) * radius,
+                y: sin(angle) * radius
+            )
+        }
+        
+    }
+}
